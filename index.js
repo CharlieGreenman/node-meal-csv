6ea6bf96103e490101ef3004ddf188436d2b7036
Charlie <charlie@Rubys-iMac-3.local>
2015-11-02 15:06:40 -0500

    file now writes csv format, need to change to three columns instead of one

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var prompt = require('prompt');
+var stringify = require('csv-stringify');
+var fs = require('fs');
+
 
 var properties = [
     {
@@ -19,9 +22,14 @@ prompt.start();
 
 prompt.get(properties, function (err, result) {
     if (err) { return onErr(err); }
+    input = [result.breakfast, result.lunch, result.dinner];
+
     console.log('  Breakfast: ' + result.breakfast);
     console.log('  Lunch: ' + result.lunch);
     console.log('  Dinner: ' + result.dinner);
+    stringify(input, function(err, output){
+      fs.writeFile('food.csv', output)
+    });
 });
 
 function onErr(err) {
